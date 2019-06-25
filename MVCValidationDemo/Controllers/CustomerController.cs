4c3616af0a2327f6ad6b8c7c7b0d808b4a068ad1
Sunny Dewangan <techsunny948@gmail.com>
2019-06-26 02:46:44 +0530

    Initial Commit

---
+++ MVCValidationDemo/Controllers/CustomerController.cs
@@ -0,0 +1,32 @@
+﻿using CustomerManagement.Models;
+using System.Web.Mvc;
+
+namespace CustomerManagement.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+        public ActionResult Display()
+        {
+            return View(new Customer());  //When the user browses the Display page directly from the browser
+        }
+
+        [HttpPost]
+        public ActionResult Submit(Customer customer)
+        {
+            if (ModelState.IsValid)   //Server side validation check
+            {
+                return View("Display", customer);
+            }
+            else
+            {
+                return View("Add");
+            }
+        }
+    }
+}
---
+++ MVCValidationDemo/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomerManagement.Models
+{
+    public sealed class Customer
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Customer code is required")]
+        [RegularExpression("^[A-Z]{3,3}[0-9]{4,4}$", ErrorMessage = "Invalid Customer Code")]  //ABC1234
+        public string CustomerCode { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Customer name is required")]
+        public string CustomerName { get; set; }
+    }
+}
